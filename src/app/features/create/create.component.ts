582a1784d85ca2317644552740e5b4920f4cd421
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-05-25 13:57:17 -0300

    feat: add msg de feedback

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -3,7 +3,11 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatButtonModule } from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input'
+import {MatSnackBar} from '@angular/material/snack-bar'
+
 import { ProductsService } from '../../shared/services/products.service';
+import { Router } from '@angular/router';
+
 
 
 
@@ -18,15 +22,31 @@ export class CreateComponent {
 
   productsService = inject(ProductsService)
 
+  matSnackBar = inject(MatSnackBar)
+
+  router = inject(Router)
+
+
+
+
+
   form = new FormGroup({
     title: new FormControl<string>('', {nonNullable:true, validators: Validators.required})
   })
 
   onSubmit(){
+
+    
+
     this.productsService.post({
       title: this.form.controls.title.value
     }).subscribe(()=> {
-      alert('Sucesso!')
+      this.matSnackBar.open('Produto criado com Sucesso!', 'Ok', {
+        duration: 300,
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      })
+      this.router.navigateByUrl('/')
     })
 
     
