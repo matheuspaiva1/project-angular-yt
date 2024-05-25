f15031ba7bfb7637f3ac37855bc412fac904aafb
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-05-25 13:47:02 -0300

    feat: ad salvar product

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input'
+import { ProductsService } from '../../shared/services/products.service';
 
 
 
@@ -14,12 +15,21 @@ import {MatInputModule} from '@angular/material/input'
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  productsService = inject(ProductsService)
+
   form = new FormGroup({
     title: new FormControl<string>('', {nonNullable:true, validators: Validators.required})
   })
 
   onSubmit(){
-    this.form.controls.title.value;
+    this.productsService.post({
+      title: this.form.controls.title.value
+    }).subscribe(()=> {
+      alert('Sucesso!')
+    })
+
+    
   }
 
 
