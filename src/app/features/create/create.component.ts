b1b4b5079b391b7ba26574688cfdbb1406ccd032
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-05-27 15:01:34 -0300

    refactor: cria component form para reutilizar codigo

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,17 +1,16 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatInputModule} from '@angular/material/input'
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {MatSnackBar} from '@angular/material/snack-bar'
 
 import { ProductsService } from '../../shared/services/products.service';
 import { Router } from '@angular/router';
+import { FormComponent } from '../../shared/components/form/form.component';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule,MatButtonModule],
+  imports: [FormComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
@@ -23,14 +22,8 @@ export class CreateComponent {
 
   router = inject(Router)
 
-  form = new FormGroup({
-    title: new FormControl<string>('', {nonNullable:true, validators: Validators.required})
-  })
-
-  onSubmit(){
-    this.productsService.post({
-      title: this.form.controls.title.value
-    }).subscribe(()=> {
+  onSubmit(product: Product) {
+    this.productsService.post(product).subscribe(()=> {
       this.matSnackBar.open('Produto criado com Sucesso!', 'Ok')
       this.router.navigateByUrl('/')
     })
